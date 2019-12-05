0776b889c987608cc5cabf30566311eedbd608be
Matt Dominick <mdominick300@gmail.com>
2019-12-04 21:22:26 -0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var searchBtn = document.querySelector('#click');
 var searchInput = document.querySelector('#inputSearch')
-var city = "Pittsburgh";
+var city = "";
 var tester = document.querySelector("#theImg1");
 var searchResult2 = document.querySelector("#section2");
 var list = document.querySelector(".list");
 var keyword = "";
 var searchBtn2 = document.querySelector('#click2');
-
+var work = 0;
+var ticketUrl = "";
+var destination = "";
+var finalDestination = document.querySelector("#end");
+var buyTickets = document.querySelector('#priceBtn');
+var getDirections = document.querySelector('#directions')
+var eventTitle = document.querySelector('.eventTitle');
 
 // function makes list of events based on city input
 function test() {
@@ -20,7 +26,7 @@ function test() {
         console.log(response);
         $('.divider').remove();
         $('.section').remove();
-    
+
 
         document.querySelector('#eventHeader').textContent = "Events in  " + city;
         for (var j = 0; j < 19; j++) {
@@ -40,13 +46,13 @@ function test() {
             p.textContent = moment(response._embedded.events[j].dates.start.localDate).format("MMM Do YYYY");
             p.setAttribute('id', 'eventDetails');
 
-            var button =document.createElement('a');
-            button.setAttribute('class','wave-effect waves-light btn modal-trigger buttonID');
+            var button = document.createElement('a');
+            button.setAttribute('class', 'wave-effect waves-light btn modal-trigger buttonID');
             button.setAttribute('href', "#modal1");
             button.text = "More Event Details";
             button.setAttribute('data-info', j);
             console.log("buttonclicked");
-            
+
 
 
             // var button = document.createElement('button');
@@ -59,16 +65,19 @@ function test() {
             console.log(j)
             document.querySelector(".list").appendChild(div).parentNode.appendChild(section).appendChild(div3).appendChild(h5).parentNode.appendChild(p).parentNode.appendChild(button).parentNode.parentNode.appendChild(div2).appendChild(img)
 
-            
+
             $('#pageHeader').text(response._embedded.events[work].name);
             $('#date').text(moment(response._embedded.events[work].dates.start.localDate).format("MMM Do YYYY"));
-            $('#price').text(response._embedded.events[work].priceRanges[0].min);
+            // $('#price').text("Price: $" + response._embedded.events[work].priceRanges[0].min + " to $" + response._embedded.events[work].priceRanges[0].max);
+            $('#city').text(moment(response._embedded.events[work].dates.start.dateTime).format("LT"));
+            $('#vlocation').text(response._embedded.events[work]._embedded.venues[0].name);
+
             // $(".modal-content").text(response._embedded.events[work].dates.start.localDate)
             // $(".modal-content").firstChild.text(response._embedded.events[work].priceRanges[0].min)
             // $(".modal-content").text(response._embedded.events[work].priceRanges[0].max)
 
         }
-        
+
 
 
     })
@@ -90,9 +99,18 @@ function test2() {
     }).then(function (response) {
         console.log(response);
         console.log(response._embedded.events[0].images[0].url);
-        document.querySelector(".large1").setAttribute("src", response._embedded.events[0].images[0].url);
-        document.querySelector(".small1").setAttribute("src", response._embedded.events[6].images[0].url);
-        document.querySelector(".small2").setAttribute("src", response._embedded.events[10].images[0].url);
+        var imgA = document.querySelector(".large1");
+        var imgB = document.querySelector(".small1");
+        var imgC = document.querySelector(".small2");
+        if (imgA) {
+            imgA.setAttribute("src", response._embedded.events[0].images[0].url);
+        }
+        if (imgB) {
+            imgB.setAttribute("src", response._embedded.events[6].images[0].url);
+        }
+        if (imgC) {
+            imgC.setAttribute("src", response._embedded.events[10].images[0].url);
+        }
     }
     )
 };
@@ -173,64 +191,69 @@ var input2 = document.querySelector("#newKeyword");
 
 
 // event listener for sport button on navbar
-sportButton.addEventListener("click", function (event) {
+if (sportButton) {
+    sportButton.addEventListener("click", function (event) {
 
-    event.preventDefault();
-    sportsContainer.classList.remove('hidden');
-    startScreenContainer.classList.add('hidden');
-    concertContainer.classList.add('hidden');
-    searchListContainer.classList.add('hidden');
-    directionsContainer.classList.add('hidden');
-    test3();
+        event.preventDefault();
+        sportsContainer.classList.remove('hidden');
+        startScreenContainer.classList.add('hidden');
+        concertContainer.classList.add('hidden');
+        searchListContainer.classList.add('hidden');
+        test3();
 
 
-});
+    })
+};
 //  event listener for concert on navbar
-concertButton.addEventListener("click", function (event) {
+if (concertButton) {
+    concertButton.addEventListener("click", function (event) {
 
-    event.preventDefault();
-    sportsContainer.classList.add('hidden');
-    concertContainer.classList.remove('hidden');
-    startScreenContainer.classList.add('hidden');
-    searchListContainer.classList.add('hidden');
-    directionsContainer.classList.add('hidden');
-    test4();
+        event.preventDefault();
+        sportsContainer.classList.add('hidden');
+        concertContainer.classList.remove('hidden');
+        startScreenContainer.classList.add('hidden');
+        searchListContainer.classList.add('hidden');
+        test4();
 
 
 
-});
+    })
+};
 
 // event listener for city searchbar
-searchBtn.addEventListener("click", function (event) {
+if (searchBtn) {
+    searchBtn.addEventListener("click", function (event) {
 
-    event.preventDefault();
+        event.preventDefault();
 
-    city = input.value;
-    console.log(city);
-    sportsContainer.classList.add('hidden');
-    concertContainer.classList.add('hidden');
-    startScreenContainer.classList.add('hidden');
-    searchListContainer.classList.remove('hidden');
-    directionsContainer.classList.add('hidden');
-    test();
+        city = input.value;
+        console.log(city);
+        sportsContainer.classList.add('hidden');
+        concertContainer.classList.add('hidden');
+        startScreenContainer.classList.add('hidden');
+        searchListContainer.classList.remove('hidden');
+        test();
 
-});
+    })
+};
 
 // event listener for keyword searchbar
-searchBtn2.addEventListener("click", function (event) {
+if (searchBtn2) {
 
-    event.preventDefault();
-    keyword = input2.value;
+    searchBtn2.addEventListener("click", function (event) {
 
-    console.log(keyword);
-    sportsContainer.classList.add('hidden');
-    concertContainer.classList.add('hidden');
-    startScreenContainer.classList.add('hidden');
-    searchListContainer.classList.remove('hidden');
-    directionsContainer.classList.add('hidden');
-    test5();
+        event.preventDefault();
+        keyword = input2.value;
 
-});
+        console.log(keyword);
+        sportsContainer.classList.add('hidden');
+        concertContainer.classList.add('hidden');
+        startScreenContainer.classList.add('hidden');
+        searchListContainer.classList.remove('hidden');
+        test5();
+
+    })
+};
 
 // creates list for keyword search results
 function test5() {
@@ -248,7 +271,7 @@ function test5() {
         document.querySelector('#eventHeader').textContent = "Results for  " + keyword;
         for (var j = 0; j < 19; j++) {
 
-
+            
             console.log(response);
             var div = document.createElement('div');
             div.setAttribute('class', "divider");
@@ -262,17 +285,33 @@ function test5() {
             var p = document.createElement("p");
             p.textContent = moment(response._embedded.events[j].dates.start.localDate).format("MMM Do YYYY");
             p.setAttribute('id', 'eventDetails');
-            var button = document.createElement('button');
-            button.setAttribute('class', 'buttonID');
-            button.textContent = "Event Details";
+
+            var button = document.createElement('a');
+            button.setAttribute('class', 'wave-effect waves-light btn modal-trigger buttonID2');
+            button.setAttribute('href', "#modal1");
+            button.text = "More Event Details";
+            button.setAttribute('data-info', j);
+            console.log("buttonclicked");
+
+
+
+            // var button = document.createElement('button');
+            // button.setAttribute('class', 'buttonID');
+            // button.textContent = "Event Details";
             var img = document.createElement("img");
             img.src = (response._embedded.events[j].images[0].url);
 
 
-
+            console.log(j)
             document.querySelector(".list").appendChild(div).parentNode.appendChild(section).appendChild(div3).appendChild(h5).parentNode.appendChild(p).parentNode.appendChild(button).parentNode.parentNode.appendChild(div2).appendChild(img)
 
-            
+
+            $('#pageHeader').text(response._embedded.events[work].name);
+            $('#date').text(moment(response._embedded.events[work].dates.start.localDate).format("MMM Do YYYY"));
+            $('#city').text(moment(response._embedded.events[work].dates.start.dateTime).format("LT"));
+            // $('#price').text("Price: $" + response._embedded.events[work].priceRanges[0].min + " to $" + response._embedded.events[work].priceRanges[0].max);
+            // $('#city').text("City: " + city);
+            $('#vlocation').text(response._embedded.events[work]._embedded.venues[0].name);
 
         }
     })
@@ -372,16 +411,107 @@ $(document).on("click", "#directions", function (event) {
 
 
 var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
+var instances = M.Modal.init(elems);
 
-    var directionsContainer = document.querySelector(".directions");
-    $(document).on("click", ".buttonID", function (event) {
-        
-       test();
-       work = $(this).attr('data-info');
-        console.log(work);
-        
-       
+var directionsContainer = document.querySelector(".directions");
+$(document).on("click", ".buttonID", function (event) {
+
+
+    test();
+    work = $(this).attr('data-info');
+    console.log(work);
+
+
+
+});
+
+var directionsContainer = document.querySelector(".directions");
+$(document).on("click", ".buttonID2", function (event) {
+
+
+    test5();
+    work = $(this).attr('data-info');
+    console.log(work);
+
+
+
+});
+
+
+
+var directionButton = document.querySelector('#directionBtn')
+
+if(directionButton){
+directionButton.addEventListener("click", function (event) {
+
+    event.preventDefault();
+    console.log("clicked")
+    location.href = 'directions.html';
+
+});
+}
+
+
+$(document).on("click", ".buttonID", function (event) {
+    console.log("click");
+    event.preventDefault();
+
+    title = $(this).attr('data-title');
+    localStorage.setItem('title', title);
+
+    // location.href = "directions.html"
+
+    destination = $(this).attr('data-dest');
+    ticketUrl = $(this).attr('data-url');
     
-    });  
+    localStorage.setItem('ticketUrl', ticketUrl);
+    localStorage.setItem('dest', destination);
+    
+    
 
+    console.log(ticketUrl);
+    console.log(destination);
+    console.log(title);
+});
+$(document).ready(function () {
+    if (buyTickets) {
+
+        $(buyTickets).on("click", function (event) {
+            console.log("click");
+            event.preventDefault();
+
+            ticketUrl = localStorage.getItem("ticketUrl");
+            window.location.href = ticketUrl;
+            console.log(ticketUrl);
+
+        })
+    }
+});
+$(document).ready(function () {
+    if (eventTitle) {
+
+        $(eventTitle).on("click", function (event) {
+            console.log("click");
+            event.preventDefault();
+            getTitle = localStorage.getItem("title");
+            $(eventTitle).innerHTML = getTitle;
+            console.log(getTitle);
+
+        })
+    }
+});
+
+
+
+$(document).ready(function () {
+    if (getDirections) {
+        $(getDirections).on('click', function (event) {
+            event.preventDefault();
+            var d = localStorage.getItem('dest')
+            $(finalDestination).attr("value", d)
+
+            // finalDestination.val(d)  ;
+            console.log(finalDestination);
+        })
+    }
+})
